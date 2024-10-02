2f23cc0df3008e6a7e648ad90840db29c97ba457
KarenEscalante <89883411+KarenEscalante@users.noreply.github.com>
2024-10-02 12:14:25 -0500

    Update ProviderApplication.java
    Se envian los eventos al topic GestiondeProductos con los mensajes de solicitud de producto y agregar nuevo producto

--- Agromarket/Provider/src/main/java/com/kafka/provider/ProviderApplication.java
+++ Agromarket/Provider/src/main/java/com/kafka/provider/ProviderApplication.java
@@ -15,11 +15,19 @@ public class ProviderApplication {
 	@Bean
 	CommandLineRunner init(KafkaTemplate<String, String> kafkaTemplate) {
 		return args-> {
-			kafkaTemplate.send( "MonitoreodeTransacciones", "Transaccion aprobada");
+			kafkaTemplate.send( "GestiondeProductos", "Hay solicitud de producto" );
+			kafkaTemplate.send( "GestiondeProductos", "Hay solicitud de agregar nuevo producto" );
+			//kafkaTemplate.send( "GestiondeUsuario", "Hay solicitud de agregar nuevo Usuario" );
+			//kafkaTemplate.send( "GestiondeUsuario", "El usuario ha sido eliminado" );
+			//kafkaTemplate.send( "ProcesamientodePedidos", "Se genera orden de compra No: 123456778");
+			//kafkaTemplate.send( "ProcesamientodePedidos", "El cliente ha hecho cambios en el pedido, ha reemplazado el producto id=1234 por producto id=5678");
+			//kafkaTemplate.send( "MonitoreodeTransacciones", "Transaccion exitosa, el pago ha sido aprobado");
+			//kafkaTemplate.send( "MonitoreodeTransacciones", "Transaccion rechazada");
 
 		};
 		
 	}
 
 }
+
 
