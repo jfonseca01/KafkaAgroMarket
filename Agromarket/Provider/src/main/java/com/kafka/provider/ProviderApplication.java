9af899b73e51cc5901d123c99739ffec2d1e85b4
Mateo01-ui <mateourbano601@gmail.com>
2024-10-02 20:29:16 -0500

    Update Provider GestiondeUsuario
    Se genera el provider de los mensajes para solicitud de agregar nuevo usuario y un usuario eliminado a servidor kafka para el topic Gestion de ususarios

--- Agromarket/Provider/src/main/java/com/kafka/provider/ProviderApplication.java
+++ Agromarket/Provider/src/main/java/com/kafka/provider/ProviderApplication.java
@@ -17,10 +17,10 @@ public class ProviderApplication {
 		return args-> {
 			//kafkaTemplate.send( "GestiondeProductos", "Hay solicitud de producto" );
 			//kafkaTemplate.send( "GestiondeProductos", "Hay solicitud de agregar nuevo producto" );
-			//kafkaTemplate.send( "GestiondeUsuario", "Hay solicitud de agregar nuevo Usuario" );
-			//kafkaTemplate.send( "GestiondeUsuario", "El usuario ha sido eliminado" );
-			kafkaTemplate.send( "ProcesamientodePedidos", "Se genera orden de compra No: 123456778");
-			kafkaTemplate.send( "ProcesamientodePedidos", "El cliente ha hecho cambios en el pedido, ha reemplazado el producto id=1234 por producto id=5678");
+			kafkaTemplate.send( "GestiondeUsuario", "Hay solicitud de agregar nuevo Usuario" );
+			kafkaTemplate.send( "GestiondeUsuario", "El usuario ha sido eliminado" );
+			//kafkaTemplate.send( "ProcesamientodePedidos", "Se genera orden de compra No: 123456778");
+			//kafkaTemplate.send( "ProcesamientodePedidos", "El cliente ha hecho cambios en el pedido, ha reemplazado el producto id=1234 por producto id=5678");
 			//kafkaTemplate.send( "MonitoreodeTransacciones", "Transaccion exitosa, el pago ha sido aprobado");
 			//kafkaTemplate.send( "MonitoreodeTransacciones", "Transaccion rechazada");
 
